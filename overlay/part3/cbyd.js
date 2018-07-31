412cf4a2b1bab18f35ff0d711b10f14952799287
rvrcamila <rivera.camila98@gmail.com>
2018-07-31 01:05:16 -0400

    fixed path to pictures

--- overlay/part3/cbyd.js
+++ overlay/part3/cbyd.js
@@ -30,24 +30,15 @@
 	function initBookmarklet($) {
 		(window.bookmarklet = function() {
 			// Replace this section with your own Information Overlay script
-			//var image = "file:///C:/Users/Camila/Sites/interactive/overlay/part2/O18-.png";
-			var imageArray = ["file:///C:/Users/Camila/Sites/interactive/overlay/part2/O18-.png", "file:///C:/Users/Camila/Sites/interactive/overlay/part2/O11-A.png", "file:///C:/Users/Camila/Sites/interactive/overlay/part2/O11-B.png", "file:///C:/Users/Camila/Sites/interactive/overlay/part2/O11-C.png", "file:///C:/Users/Camila/Sites/interactive/overlay/part2/O31-A.png", "file:///C:/Users/Camila/Sites/interactive/overlay/part2/O5-.png", "file:///C:/Users/Camila/Sites/interactive/overlay/part2/O4-.png"];
+			var imageArray = ["https://rvrcamila.github.io/art167/overlay/part2/O18-.png", "https://rvrcamila.github.io/art167/overlay/part2/O11-A.png", "https://rvrcamila.github.io/art167/overlay/part2/O11-B.png", "https://rvrcamila.github.io/art167/overlay/part2/O11-C.png", "https://rvrcamila.github.io/art167/overlay/part2/O31-A.png", "https://rvrcamila.github.io/art167/overlay/part2/O5-.png", "https://rvrcamila.github.io/art167/overlay/part2/O4-.png"];
 			var i = Math.floor(Math.random() * imageArray.length);
 			var x = Math.floor(Math.random() * 100);
 			var y = Math.floor(Math.random() * 100);
 			var size =  Math.floor(Math.random() * 20) + 10;
 
-			//$('head').append('<style> #overlay-image {all:none; position:fixed; top:15px; left: 30px;} </style>');
 			$(":not(.overlay-image)").css("background-color", "black");
 			$("*").css("color", "white");
 	    $("body").append('<img class ="overlay-image" style="all:none; width:'+ size +'vw; position:fixed; left:' + x + 'vw; top:'+ y +'vh;" src="' + imageArray[i] + '" />');
-			//$(".overlay-image").css("all", "none");
-			//$(".overlay-image").css("position", "fixed");
-			//$(".overlay-image").css("top", "300px");
-			//$(".overlay-image").css("left","300px");
-
-
-
 		})();
 	}
 })();
