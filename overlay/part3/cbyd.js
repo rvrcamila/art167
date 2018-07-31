fbfc41e483b9a3c61dfa2a99774257f4ccaf47e5
rvrcamila <rivera.camila98@gmail.com>
2018-07-31 14:36:53 -0400

    all:unset instead of none for the overlayed images

--- overlay/part3/cbyd.js
+++ overlay/part3/cbyd.js
@@ -38,7 +38,7 @@
 
 			$(":not(.overlay-image)").css("background-color", "black");
 			$("*").css("color", "white");
-	    $("body").append('<img class ="overlay-image" style="all:none; width:'+ size +'vw; position:fixed; left:' + x + 'vw; top:'+ y +'vh;" src="' + imageArray[i] + '" />');
+	    $("body").append('<img class ="overlay-image" style="all:unset; width:'+ size +'vw; position:absolute; left:' + x + 'vw; top:'+ y +'vh;" src="' + imageArray[i] + '" />');
 		})();
 	}
 })();
